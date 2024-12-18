20d9b3e5983cda54b4c6cd035ab4b20dcbd0cf11
Geevarghese George <geevarghese.george@avl.com>
2024-12-18 17:31:56 +0100

    add day7-tmp

---
+++ src/bin/day7.rs
@@ -0,0 +1,23 @@
+/* 
+
+*/
+
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    collections::{HashSet, HashMap}, 
+}; 
+use regex::Regex;
+
+
+fn main(){ 
+    
+    let input_result = File::open("data/day7_a.txt");
+    let input = match input_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+    println!("Contents of file: {:?}", input);
+    let mut reader = BufReader::new(input);   
+    
+}
